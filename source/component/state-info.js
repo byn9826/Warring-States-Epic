21162a6cefda7403cea1e29a79a362e73f55acb6
Baozier <byn9826@gmail.com>
2017-09-03 01:52:35 +0000

    add states board

---
+++ source/component/state-info.js
@@ -0,0 +1,103 @@
+Vue.component("state-info", {
+    props: ["definition", "player", "data", "power"],
+    template: `
+        <div v-bind:style="cardStyle">
+            <div v-bind:style="headerStyle">
+                <span v-bind:style="player?activeStyle:nameStyle">
+                    {{definition.name}}
+                </span>
+                <span v-bind:style="occupyStyle">
+                    都会 {{data.capital.length}} 城市 {{data.city.length}} 国库 {{power[definition.code]}}
+                </span>
+            </div>
+            <div v-bind:style="strengthStyle">
+                <span v-bind:style="armyStyle">
+                    兵力{{data.army.length}}
+                    <span v-if="player">
+                        - {{getArmyDetail()}}
+                    </span>
+                </span>
+            </div>
+        </div>
+    `,
+    methods: {
+        getArmyDetail() {
+            var a = 0, b = 0, c = 0, d = 0;
+            this.data.army.forEach(function(type) {
+                switch(type) {
+                    case 0:
+                        a += 1;
+                        break;
+                    case 8:
+                        b += 1;
+                        break;
+                    case "9":
+                        c += 1;
+                        break;
+                    default:
+                        d += 1;
+                        break;
+                }
+            }.bind(this));
+            return this.getArmyInfo()[0].name + " " + a + " " 
+                + this.getArmyInfo()[8].name + " " + b + " " 
+                + this.getArmyInfo()[9].name + " " + c + " "
+                + this.getArmyInfo()[this.definition.code].name + " " + d;
+        }
+    },
+    data: function() {
+        return {
+            control: [],
+            cardStyle: {
+                display: "block",
+                padding: "3px",
+                borderBottom: "3px solid black",
+                borderRadius: "5px"
+            },
+            headerStyle: {
+                display: "block",
+                borderBottom: "1px dashed lightgrey",
+                width: "80%",
+                paddingBottom: "3px"
+            },
+            nameStyle: {
+                display: "inline-block",
+                verticalAlign: "middel",
+                backgroundColor: "lightgrey",
+                fontSize: "14px",
+                minWidth: "20px",
+                height: "20px",
+                lineHeight: "20px",
+                borderRadius: "50%",
+                textAlign: "center"
+            },
+            activeStyle: {
+                display: "inline-block",
+                verticalAlign: "middel",
+                backgroundColor: this.definition.color,
+                color: "white",
+                fontSize: "14px",
+                minWidth: "20px",
+                height: "20px",
+                lineHeight: "20px",
+                borderRadius: "50%",
+                textAlign: "center"
+            },
+            occupyStyle: {
+                display: "inline-block",
+                verticalAlign: "middel",
+                color: "lightgrey",
+                fontSize: "12px",
+            },
+            strengthStyle: {
+                display: "block"
+            },
+            armyStyle: {
+                display: "inline-block",
+                verticalAlign: "middel",
+                color: "whitesmoke",
+                fontSize: "12px",
+            },
+        }  
+    },
+});
--- source/epic.js
+++ source/epic.js
@@ -1,9 +1,41 @@
 var app = new Vue({
     el: '#app',
     data: {
-        cities: data.cities
+        cities: data.cities,
+        player: data.player,
+        power: data.power
     },
-    methods: {
+    computed: {
+        statesInfo: function() {
+            var states = [], i = 0;
+            for (i; i < this.getStatesInfo().length; i++) {
+                states[i] = {
+                    capital: [],
+                    city: [],
+                    army: []
+                }
+            }
+            this.cities.forEach(function(city) {
+                if (this.getCitiesInfo()[city.code].type === 0) {
+                    states[city.occupy].capital.push(city.code);
+                } else if (this.getCitiesInfo()[city.code].type === 1) {
+                    states[city.occupy].city.push(city.code);
+                }
+                states[city.occupy].army = states[city.occupy].army.concat(city.army);
+            }.bind(this));
+            return states;
+            
+    
+            var total = 0;
+            this.cities.forEach(function(c) {
+                if (c.occupy === this.definition.code) {
+                    total += c.army.length;
+                }
+            }.bind(this));
+            return total;
 
+            
+            
+        }
     }
 });
