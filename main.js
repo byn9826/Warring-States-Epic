12fc11f9c14ee07235b294960105cf336e9974d9
Yinan Bao <byn9826@gmail.com>
2017-10-22 00:25:25 -0400

    use electron

---
+++ main.js
@@ -0,0 +1,36 @@
+const electron = require( 'electron' );
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+const path = require( 'path' );
+const url = require( 'url' );
+let mainWindow;
+
+app.on('ready', () => {
+    mainWindow = new BrowserWindow({
+        height: 600,
+        width: 800
+    });
+    //mainWindow.setMenu( null );
+    mainWindow.maximize();
+    mainWindow.loadURL( url.format({
+        pathname: path.join( __dirname, './game.html' ),
+        protocol: 'file:',
+        slashes: true
+    }));
+    mainWindow.on('closed', () => {
+        mainWindow = null;
+    });
+});
+
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', function () {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
